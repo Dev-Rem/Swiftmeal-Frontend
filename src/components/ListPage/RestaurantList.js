fd04eed59587e190f8f7146047cb593a7e145d97
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2023-06-30 16:06:50 +0100

    Add restaurant card for the Restuarant list page

--- src/components/ListPage/RestaurantList.js
+++ src/components/ListPage/RestaurantList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Heading } from "@chakra-ui/react";
+import { Card, Heading, Image, Text } from "@chakra-ui/react";
 
 export const RestaurantList = () => {
   return (
@@ -13,12 +13,48 @@ export const RestaurantList = () => {
         Restaurants - {"Nigeria"}
       </Heading>
 
-      <div className="category-container">
-
-      </div>
+      <div className="category-container"></div>
 
       <div className="restaurant-list">
-        
+        <Card maxW="sm" height="100%" borderRadius={20}>
+          <Image
+            src={require("../../assets/images/sweets.jpeg")}
+            alt="Green double couch with wooden legs"
+            borderTopRadius="lg"
+            height={200}
+          />
+          <div style={{ padding: 15 }}>
+            <Heading
+              size="md"
+              color="#b503a6"
+              fontFamily={("Tangerine", "cursive")}
+            >
+              Pizza Hut
+            </Heading>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <div>
+                <p>Address:</p>
+                <p>Min order:</p>
+              </div>
+              <div
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  backgroundColor: "#f4f4f4",
+                  borderRadius: 10,
+                  borderWidth: "2px",
+                  borderColor: "#b503a6",
+                }}
+              ></div>
+            </div>
+          </div>
+        </Card>
       </div>
     </div>
   );
