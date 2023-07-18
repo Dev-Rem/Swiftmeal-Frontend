8068f98fbeea403768fedd250d6798c1dbdcbbe8
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2023-07-18 16:09:02 +0100

    Feat: Clear localstorage cache when token expires

--- src/components/utils/AxiosInstance.js
+++ src/components/utils/AxiosInstance.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const axiosInstance = axios.create(
   {
@@ -11,19 +12,21 @@ export const axiosInstance = axios.create(
   },
   { withCredentials: true }
 );
-
 axiosInstance.interceptors.response.use(
   (response) => {
     return response;
   },
   (error) => {
-    if (error.response && error.response.status === 401) {
+    const navigate = useNavigate();
+
+    if ((error.response && error.response.status === 401) || 400) {
       // Check if the response status is 401 (Unauthorized)
       // Handle token expiration or unauthorized access here
       // For example, redirect to a login page or show an error message
-      localStorage.setItem("authorization", "");
-      localStorage.setItem("signedIn", false);
-      console.log("Token has expired or unauthorized access");
+      localStorage.removeItem("authorization");
+      localStorage.removeItem("signedIn");
+      localStorage.removeItem("userInfo");
+      navigate("/");
     }
     return Promise.reject(error);
   }
